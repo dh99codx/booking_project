49a248ac868c747c06db24e61b3aaf9f0f552706
Dhanushka <37043938+dhanush167@users.noreply.github.com>
2024-02-03 07:49:48 +0530

    5.form page changes

--- resources/views/app/user_profiles/form-inputs.blade.php
+++ resources/views/app/user_profiles/form-inputs.blade.php
@@ -24,6 +24,16 @@
     </x-inputs.group>
 
     <x-inputs.group class="col-sm-12">
+        <x-inputs.select name="user_id" label="User" required>
+            @php $selected = old('user_id', ($editing ? $userProfile->user_id : '')) @endphp
+            <option disabled {{ empty($selected) ? 'selected' : '' }}>Please select the User</option>
+            @foreach($users as $value => $label)
+            <option value="{{ $value }}" {{ $selected == $value ? 'selected' : '' }} >{{ $label }}</option>
+            @endforeach
+        </x-inputs.select>
+    </x-inputs.group>
+
+    <x-inputs.group class="col-sm-12">
         <x-inputs.text
             name="rashi"
             label="Rashi"
@@ -43,46 +53,5 @@
             placeholder="Natchatram"
             required
         ></x-inputs.text>
-    </x-inputs.group>
-
-    <x-inputs.group class="col-sm-12">
-        <div
-            x-data="imageViewer('{{ $editing && $userProfile->profile_picture ? \Storage::url($userProfile->profile_picture) : '' }}')"
-        >
-            <x-inputs.partials.label
-                name="profile_picture"
-                label="Profile Picture"
-            ></x-inputs.partials.label
-            ><br />
-
-            <!-- Show the image -->
-            <template x-if="imageUrl">
-                <img
-                    :src="imageUrl"
-                    class="object-cover rounded border border-gray-200"
-                    style="width: 100px; height: 100px;"
-                />
-            </template>
-
-            <!-- Show the gray box when image is not available -->
-            <template x-if="!imageUrl">
-                <div
-                    class="border rounded border-gray-200 bg-gray-100"
-                    style="width: 100px; height: 100px;"
-                ></div>
-            </template>
-
-            <div class="mt-2">
-                <input
-                    type="file"
-                    name="profile_picture"
-                    id="profile_picture"
-                    @change="fileChosen"
-                />
-            </div>
-
-            @error('profile_picture')
-            @include('components.inputs.partials.error') @enderror
-        </div>
     </x-inputs.group>
 </div>
